14df34255c868347665fb8c1c607c286ce3ba3ed
Piet Brauer <piet@nerdishbynature.com>
2014-08-06 21:46:32 +0200

    Add simple list

--- RealmToDo/ViewController.swift
+++ RealmToDo/ViewController.swift
@@ -1,6 +1,25 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    
+    var todos: [String] {
+        get {
+            return ["Milk", "Eggs"]
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "CellIdentifier")
+    }
+
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = todos[indexPath.row]
+        return cell
+    }
 }
 
